f432b89a1c56ad3feb7ce2dfbf065d22234f7baf
PauloBigooD <53543357+PauloBigooD@users.noreply.github.com>
2025-01-17 14:50:05 -0300

    Update config.py

--- config.py
+++ config.py
@@ -12,15 +12,11 @@ def tensorboard_cleanup():
     shutil.rmtree(info_dir)
 
 FOLDERS = {
-    6: ['plots','stepbystep','stepbystep','data_generation', 'data_preparation']  # Add chapter 6 entr
+    6: ['plots']  # Add chapter 6 entr
 }
 FILENAMES = {
         6: [ # Add filenames for chapter 6 here
-            'plots/chapter6.py',
-            'stepbystep/v2.py',
-            'stepbystep/v3.py',
-            'data_generation/simple_linear_regression.py',
-            'data_preparation/v2.py'   
+            'plots/chapter6.py',   
        ]
 }
 try:
